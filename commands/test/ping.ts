c588133858ef01d6e144463359687027910b526d
Ammarcodex <ammarcodex@gmail.com>
2023-04-04 19:57:51 +0500

    git

--- commands/test/ping.ts
+++ commands/test/ping.ts
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('wing')
+		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
