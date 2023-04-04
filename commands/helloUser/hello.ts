35bea10ebdac7c718cfc973d4edc6b57a8fef852
Ammarcodex <ammarcodex@gmail.com>
2023-04-04 23:17:50 +0500

    some changes to blackjack

--- commands/helloUser/hello.ts
+++ commands/helloUser/hello.ts
@@ -6,7 +6,22 @@ module.exports = {
 		.setName('blackjack')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-        let num = Math.floor(Math.random() * 8); 
-		await interaction.reply(num.toString());
+
+        let Card_1 = Math.floor(Math.random() * 21)
+		let Card_2 = Math.floor(Math.random() * 21)
+		let Card_3 = Math.floor(Math.random() * 21)
+		console.log(Card_1+ " " + Card_2);
+		let sum = Card_1 + Card_2;
+		if(sum == 21){
+
+			await interaction.reply("  Card_1 : "+Card_1.toString()+"  Card 2 : "+Card_2.toString()+" The Sum of your Card is " +sum.toString()+" You have won the Blackjack");
+		} 
+		else if (sum! == 21)
+		{
+		 await interaction.reply("You will have to pick a card")
+
+		}
+		
+		
 	},
 };
