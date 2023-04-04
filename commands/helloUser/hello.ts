730aec9a57f3c061af105726b4c06aa032fe8677
Ammarcodex <ammarcodex@gmail.com>
2023-04-04 23:58:28 +0500

    update

--- commands/helloUser/hello.ts
+++ commands/helloUser/hello.ts
@@ -10,16 +10,14 @@ module.exports = {
 		let Card_2 = Math.floor(Math.random() * 21)
 		let Card_3 = Math.floor(Math.random() * 21)
 		console.log(Card_1+ " " + Card_2);
-		let sum = Card_1 + Card_2;
+		let sum = 21 //Card_1 + Card_2;
+		console.log(sum);
 		if(sum == 21){
 
 			await interaction.reply("  Card_1 : "+Card_1.toString()+"  Card 2 : "+Card_2.toString()+" The Sum of your Card is " +sum.toString()+" You have won the Blackjack");
 		} 
-		else if (sum !== 21)
-		{
-		 await interaction.reply("You will have to pick a card")
 
-		}
+		
 		
 		
 	},
