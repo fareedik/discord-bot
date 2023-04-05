c49af4553ba92e142d63fc1abd166b4280565fd8
Wajahat Baig <waji.baig11@gmail.com>
2023-04-05 14:55:58 +0500

    Black jack organized

--- commands/basicCardGame/BlackJackBasic.ts
+++ commands/basicCardGame/BlackJackBasic.ts
@@ -5,25 +5,29 @@ module.exports = {
     .setName('blackjack2')
     .setDescription('Play a game of Blackjack'),
   async execute(interaction) {
+
+  // Deck of cards
    const Deck = [
       "A♠️","2♠️","3♠️","4♠️","5♠️","6♠️","7♠️","8♠️","9♠️","10♠️","J♠️","Q♠️","K♠️",
       "A♥️","2♥️","3♥️","4♥️","5♥️","6♥️","7♥️","8♥️","9♥️","10♥️","J♥️","Q♥️","K♥️",
       "A♣️","2♣️","3♣️","4♣️","5♣️","6♣️","7♣️","8♣️","9♣️","10♣️","J♣️","Q♣️","K♣️",
       "A♦️","2♦️","3♦️","4♦️","5♦️","6♦️","7♦️","8♦️","9♦️","10♦️","J♦️","Q♦️","K♦️"
     ];
-    
+
+  // Shuffle the deck
     const shuffle = (array) => {
       for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
       }
     };
-
     shuffle(Deck);
 
+    // Player and dealer hands
     const playerHand = [Deck[0], Deck[2]];
     const dealerHand = [Deck[1], Deck[3]];
 
+    // Calculate the score of the player's hand
     const playerScore = () => {
       let score = 0;
       let hasAce = false;
@@ -49,6 +53,7 @@ module.exports = {
       return score;
     };
 
+    // Calculate the score of the dealer's hand
     const dealerScore = () => {
       let score = 0;
       let hasAce = false;
@@ -74,6 +79,7 @@ module.exports = {
       return score;
     };
 
+    // Convert the player's hand to a string
     const playerString = () => {
       let handString = "";
       for (const card of playerHand) {
@@ -82,6 +88,7 @@ module.exports = {
       return handString;
     }
 
+    // Convert the dealer's hand to a string
     const dealerString = () => {
       let handString = "";
       for (const card of dealerHand) {
@@ -90,11 +97,13 @@ module.exports = {
       return handString;
     }
 
+    // Hit function
     const hit = () => {
       playerHand.push(Deck[Deck.length - 1]);
       Deck.pop();
     }
 
+    // Dealer's turn
     const dealerTurn = () => {
       while (dealerScore() < 17) {
         dealerHand.push(Deck[Deck.length - 1]);
@@ -102,6 +111,7 @@ module.exports = {
       }
     }
 
+    // Check who won
     const checkWin = () => {
       if (playerScore() > 21) {
         return "You busted! You lose!";
@@ -120,10 +130,18 @@ module.exports = {
       }
     }
 
+    // Start the game
     await interaction.reply(`Your hand: ${playerString()}\nDealer hand: ${dealerHand[0]} **?**\nYour score: ${playerScore()}\nDealer score: ${dealerScore()}`);
+    // Prompt the user to hit or stand
     await interaction.reply(`Hit or stand?`);
+
+    // Create a message collector
     const filter = m => m.author.id === interaction.user.id;
+
+    // Collect the user's response
     const collector = interaction.channel.createMessageCollector(filter, { time: 15000 });
+
+    // Check the user's response
     collector.on('collect', m => {
       if (m.content.toLowerCase() === 'hit') {
         hit();
@@ -137,6 +155,8 @@ module.exports = {
         interaction.reply(`Please enter 'hit' or 'stand'`);
       }
     });
+
+    // If the user doesn't respond in time
     collector.on('end', collected => {
       if (collected.size === 0) {
         interaction.reply('You took too long to respond!');
